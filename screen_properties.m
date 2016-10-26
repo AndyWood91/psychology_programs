b519de905d3e6cb81d10e30a042178b62f00c8ce
Andy Wood <awoo5332@gmail.com>
2016-10-26 11:12:17 +1100

    screen props

--- screen_properties.m
+++ screen_properties.m
@@ -1,2 +1,9 @@
-screen_number = 0;  % primary monitor
-[screen_width, screen_height] = Screen('WindowSize', screen_number);
+function [screen_width, screen_height] = screen_properties(screen_number)
+    
+    if nargin < 1
+        screen_number = 0;  % primary monitor
+    end
+    
+    [screen_width, screen_height] = Screen('WindowSize', screen_number);
+    
+end
